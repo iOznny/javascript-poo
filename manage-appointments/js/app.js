5e1504067ed1458952b64bb5abc9d9228b090e82
iOznny <f000170014@gmail.com>
2022-01-09 14:05:01 -0600

    c

---
+++ manage-appointments/js/app.js
@@ -0,0 +1,22 @@
+// Inputs
+const petInput = document.querySelector('#mascota');
+const ownerInput = document.querySelector('#propietario');
+const phoneInput = document.querySelector('#telefono');
+const dateInput = document.querySelector('#fecha');
+const hourInput = document.querySelector('#hors');
+const contentInput = document.querySelector('#sintomas');
+
+const form = document.querySelector('#nueva-cita');
+
+// Content List
+const container = document.querySelector('#citas');
+
+// Run App
+eventListeners();
+function eventListeners() {
+    petInput.addEventListener('change', dataQuote);
+}
+
+function dataQuote(e) {
+    console.log(e.target.value);
+}
