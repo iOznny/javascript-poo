e20f971616feaf9a7aa4f38ed2c372d2ca28d1a7
awdawd <awdawd@g.com>
2022-01-20 20:39:24 -0600

    Changes

--- manage-appointments/js/app.js
+++ manage-appointments/js/app.js
@@ -12,6 +12,22 @@ const form = document.querySelector('#nueva-cita');
 // Content List
 const container = document.querySelector('#citas');
 
+// Class Quote & UI
+class Quote {
+    constructor() {
+        this.quotes = [];
+    }
+}
+
+class UI {
+    
+}
+
+const quote = new Quote();
+const ui = new UI();
+
+
+
 // Run App
 eventListeners();
 function eventListeners() {
@@ -20,6 +36,8 @@ function eventListeners() {
     phoneInput.addEventListener('input', dataQuote);
     dateInput.addEventListener('input', dataQuote);
     hourInput.addEventListener('input', dataQuote);
+
+    form.addEventListener('submit', newQuote());
 }
 
 // OBject Quote
@@ -36,4 +54,18 @@ const quoteObj = {
 function dataQuote(e) {    
     quoteObj[e.target.name] = e.target.value;
     console.log(quoteObj);
+}
+
+// Checking and adding new quote 
+function newQuote(e) {
+    e.preventDefault();
+
+    // Information of quote
+    const { pet, owner, phone, date, hour, content } = quoteObj;
+
+    // Valid
+    if (pet === '' || owner === '' || phone === '' || date === '' || hour === '' || content === '') {
+        console.log('All items is required');
+        return;
+    }
 }
